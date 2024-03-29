f93641010b6dd45188a216adab6965ec06d37bc7
Sarah Mancho <srhman@terpmail.umd.edu>
2024-03-28 23:18:34 -0400

    Add the favorite course and contact route with their user request to the Python file. Turned debug on.
    Create the contact.html file and added a series of user request to track Name, Email, and Phone Number.
    
    Add the favorite-course.html with user request on subject and course number.
    
    Create index.html file to incorporate bootstrap.
    
    Add styles to style.css to modify contact.html

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -15,6 +15,19 @@ def about():
 @app.route('/about-css/')
 def about_css():
     return render_template('about-css.html')
+@app.route('/favorite-course', methods=['GET','POST'])
+def favorite_course():
+    print(f'Subject: {request.form.get("subject")}')
+    print(f'Course_number: {request.form.get("course_number")}')
+    return render_template('favorite-course.html')
+@app.route('/contact')
+def contact():
+    if request.method == 'POST':
+        return render_template('contact.html', form_submitted=True)
+    else:
+        return render_template('contact.html')
+
 
 if __name__ == '__main__':
+    app.debug = True
     app.run()
